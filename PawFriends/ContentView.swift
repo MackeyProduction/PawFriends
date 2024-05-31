bb305218d55d7c2fd4c98da68b1e04ab2561d80e
til <til.anheier@outlook.de>
2024-05-31 18:19:06 +0200

    Add TabView and different views for TabView.

--- PawFriends/ContentView.swift
+++ PawFriends/ContentView.swift
@@ -2,60 +2,37 @@
 //  ContentView.swift
 //  PawFriends
 //
-//  Created by Til Anheier on 29.05.24.
+//  Created by Til Anheier on 31.05.24.
 //
 
 import SwiftUI
-import SwiftData
 
 struct ContentView: View {
-    @Environment(\.modelContext) private var modelContext
-    @Query private var items: [Item]
-
     var body: some View {
-        NavigationSplitView {
-            List {
-                ForEach(items) { item in
-                    NavigationLink {
-                        Text("Item at \(item.timestamp, format: Date.FormatStyle(date: .numeric, time: .standard))")
-                    } label: {
-                        Text(item.timestamp, format: Date.FormatStyle(date: .numeric, time: .standard))
-                    }
+        TabView {
+            SearchView()
+                .tabItem {
+                    Label("Suche", systemImage: "magnifyingglass")
                 }
-                .onDelete(perform: deleteItems)
-            }
-            .toolbar {
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    EditButton()
+            
+            FavoriteView()
+                .tabItem {
+                    Label("Favoriten", systemImage: "heart")
                 }
-                ToolbarItem {
-                    Button(action: addItem) {
-                        Label("Add Item", systemImage: "plus")
-                    }
+            
+            MessageView()
+                .tabItem {
+                    Label("Nachrichten", systemImage: "message")
                 }
-            }
-        } detail: {
-            Text("Select an item")
-        }
-    }
-
-    private func addItem() {
-        withAnimation {
-            let newItem = Item(timestamp: Date())
-            modelContext.insert(newItem)
-        }
-    }
-
-    private func deleteItems(offsets: IndexSet) {
-        withAnimation {
-            for index in offsets {
-                modelContext.delete(items[index])
-            }
+            
+            ProfileView()
+                .tabItem {
+                    Label("Profil", systemImage: "person")
+                }
         }
     }
 }
 
 #Preview {
     ContentView()
-        .modelContainer(for: Item.self, inMemory: true)
 }
---
+++ PawFriends/ProfileView.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileView.swift
+//  PawFriends
+//
+//  Created by Til Anheier on 31.05.24.
+//
+
+import SwiftUI
+
+struct ProfileView: View {
+    var body: some View {
+        Text("Hello, this is the profile view.")
+    }
+}
+
+#Preview {
+    ProfileView()
+}
