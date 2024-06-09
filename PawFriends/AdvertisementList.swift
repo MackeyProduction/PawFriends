aaa50a95f54a60c4ccd542335c361c08630048e7
til <til.anheier@outlook.de>
2024-06-09 14:02:30 +0200

    Rename Item to Advertisement. Add search function. Refactor code.

---
+++ PawFriends/AdvertisementList.swift
@@ -0,0 +1,18 @@
+//
+//  AdvertisementList.swift
+//  PawFriends
+//
+//  Created by Til Anheier on 09.06.24.
+//
+
+import SwiftUI
+
+struct AdvertisementList: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AdvertisementList()
+}
