599b74cb1457f8915fd91addd68ed88f50237254
til <til.anheier@outlook.de>
2024-06-10 12:03:29 +0200

    Add style for profile view.

--- PawFriends/ProfileView.swift
+++ PawFriends/ProfileView.swift
@@ -9,15 +9,107 @@ import SwiftUI
 
 struct ProfileView: View {
     var body: some View {
-        NavigationSplitView {
-            Group {
-                Text("Hello, this is the profile view.")
+        VStack {
+            HStack {
+                Text("Profil")
+                    .font(.largeTitle)
+                    .bold()
+                
+                Spacer()
             }
-            .navigationTitle("Profil")
-        } detail: {
-            Text("Select an item")
-                .navigationTitle("Profil")
+            
+            HStack {
+                Image(systemName: "person.fill")
+                    .resizable()
+                    .frame(width: 100, height: 100, alignment: .center)
+                    .clipShape(Circle())
+                
+                VStack(alignment: .leading) {
+                    Text("Markus")
+                        .font(.largeTitle)
+                    
+                    HStack {
+                        Image(systemName: "bookmark")
+                        Text("Aktiv seit: xx.xx.xxxx")
+                            .foregroundStyle(.secondary)
+                    }
+                }
+            }
+            
+            HStack {
+                Image(systemName: "person.3.fill")
+                Text("2 Follower")
+                Spacer()
+            }
+            .padding([.top, .bottom], 5)
+            
+            HStack {
+                Image(systemName: "bookmark")
+                Text("Tags")
+                Spacer()
+            }
+            .padding([.top, .bottom], 5)
         }
+        .padding(.all)
+        .frame(maxWidth: .infinity)
+        .background(.green)
+        .foregroundStyle(.white)
+        
+        VStack {
+            HStack {
+                Text("Beschreibung")
+                    .font(.largeTitle)
+                
+                Spacer()
+                
+                Image(systemName: "pencil")
+            }
+            
+            HStack {
+                Text("Lorem Ipsum")
+                Spacer()
+            }
+            
+        }
+        .padding(.all)
+        
+        VStack {
+            HStack {
+                Text("Tiere")
+                    .font(.largeTitle)
+                
+                Spacer()
+                
+                Image(systemName: "plus.square")
+            }
+            
+            HStack {
+                Text("Lorem Ipsum")
+                Spacer()
+            }
+            
+        }
+        .padding(.all)
+        
+        VStack {
+            HStack {
+                Text("Anzeigen")
+                    .font(.largeTitle)
+                
+                Spacer()
+                
+                Image(systemName: "plus.square")
+            }
+            
+            HStack {
+                Text("Lorem Ipsum")
+                Spacer()
+            }
+            
+        }
+        .padding(.all)
+        
+        Spacer()
     }
 }
 
