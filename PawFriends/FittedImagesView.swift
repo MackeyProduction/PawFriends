c4a01584be310bcaa88b08983758d6fe31430faf
30044 <hanna.steffen@hsrw.org>
2024-06-20 18:02:42 +0200

    uff

---
+++ PawFriends/FittedImagesView.swift
@@ -0,0 +1,18 @@
+//
+//  FittedImagesView.swift
+//  PawFriends
+//
+//  Created by Hanna Steffen on 15.06.24.
+//
+
+import SwiftUI
+
+struct FittedImagesView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FittedImagesView()
+}
---
+++ PawFriends/PhotoPickerView.swift
@@ -0,0 +1,18 @@
+//
+//  PhotoPickerView.swift
+//  PawFriends
+//
+//  Created by Hanna Steffen on 15.06.24.
+//
+
+import SwiftUI
+
+struct PhotoPickerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PhotoPickerView()
+}
